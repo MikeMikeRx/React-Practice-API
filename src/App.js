98b01c7018ff417b4f35ac00c97a6d579dc978b3
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 09:54:35 +0100

    ISS API created, added function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { useState, useEffect } from "react"
-
 const App = () => {
-  const [quote, setQuote] = useState ("default Text")
-  
-  const url = "https://api.kanye.rest/"
+  const url = "http://api.open-notify.org/iss-now.json"
 
-  useEffect(()=>{
-  },[])
-
-  const getQuote = async () =>{
-    const response = await fetch(url)
-    const data = await response.json()
-    const finalQuote = data["quote"]
-    setQuote(finalQuote)
+  const getCoordinates = () => {
+    const response = fetch (url)
+    const data = response.json()
   }
 
-  useEffect(()=>{
-    getQuote()
-  },[]) //<--- Prevent refreshing 
-  
-  
-  
   return (
-    <div>
-      <h1>{quote}</h1>
-   </div> 
+    <h1>API</h1>
   )
 }
-
-
+  
+  
+  
 export default App
