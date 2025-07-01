55e6c6d22c9f520cbe59156d02384b57299acded
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 10:01:01 +0100

    Added async JS, getting data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
 
-  const getCoordinates = () => {
-    const response = fetch (url)
-    const data = response.json()
+  const getCoordinates = async() => {
+    const response = await fetch (url)
+    const data = await response.json()
+    console.log(data["iss_position"]["latitude"])
+    console.log(data["iss_position"]["longitude"])
   }
+
+  getCoordinates()
 
   return (
     <h1>API</h1>
