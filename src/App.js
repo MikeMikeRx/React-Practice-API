f08da8c82a8027e4fe28181d518d3ca6e0245c9b
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 10:10:52 +0100

    Changed const to let, added <h2> and <p>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
-  const latitude = ""
-  const longitude = ""
+  let latitude = ""
+  let longitude = ""
 
   const getCoordinates = async() => {
     const response = await fetch (url)
     const data = await response.json()
-    console.log(typeof(data["iss_position"]["latitude"]))
+    console.log(data["iss_position"]["latitude"])
     console.log(data["iss_position"]["longitude"])
+    latitude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
   }
 
   getCoordinates()
 
   return (
-    <h1>API</h1>
+    <div>
+    <h1>International Space Station</h1>
+    <h2>Latitude</h2>
+    <p>{latitude}</p>
+    <h2>Longitude</h2>
+    <p>{longitude}</p>
+    </div>
   )
 }
   
