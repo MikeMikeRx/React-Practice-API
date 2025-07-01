d37ca9ec2be7e9643629337ee45cb805dae5959b
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 12:34:01 +0100

    Added <h2>, <p> and setLatitude/Longitude

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ const url = "http://api.open-notify.org/iss-now.json"
 
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [latitude, setLatitude] = useState ("")
+  const [longitude, setLongitude] = useState ("")
 
   useEffect(()=>{
     fetch(url)
       .then( (response) => response.json() )
       .then( (data) => data["iss_position"] )
       .then( (position) => {
-                console.log(position["latitude"])
-                console.log(position["longitude"])
+                // console.log(position["latitude"])
+                // console.log(position["longitude"])
+                setLatitude(position["latitude"])
+                setLongitude(position["longitude"])
       })
   },[])
 
-
   if (loading){
-    return <h2>Loading ....</h2>
-  } else {
-    return <h2>Website content</h2>
-  }
+    return <h2>Loading Website...</h2>
+  } 
+  
+  return <>
+    <h2>Latitude:</h2>
+    <p>{latitude}</p>
+    <h2>Longitude:</h2>
+    <p>{longitude}</p>
+  </>
+
   
 }
 
