e692115893d3a55fbcf43e6bbf2f7d3dcd8c2931
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 10:19:26 +0100

    Used useState to show data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState, useEffect } from "react"
+
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
-  let latitude = ""
-  let longitude = ""
+  const [latitude, setLatitude] = useState ("")
+  const [longitude, setLongitude] = useState ("")
 
   const getCoordinates = async() => {
     const response = await fetch (url)
     const data = await response.json()
-    console.log(data["iss_position"]["latitude"])
-    console.log(data["iss_position"]["longitude"])
-    latitude = data["iss_position"]["latitude"]
-    longitude = data["iss_position"]["longitude"]
+    // console.log(data["iss_position"]["latitude"])
+    // console.log(data["iss_position"]["longitude"])
+    setLatitude(data["iss_position"]["latitude"])
+    setLongitude(data["iss_position"]["longitude"])
   }
 
   getCoordinates()
 
   return (
     <div>
-    <h1>International Space Station</h1>
+    <h1>International Space Station:</h1>
     <h2>Latitude</h2>
     <p>{latitude}</p>
     <h2>Longitude</h2>
