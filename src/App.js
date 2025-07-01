f6e91860f36d842d058f45ef4a9dc099f8bbccc7
MichalRais <Michal.Rais@iCloud.com>
2025-07-01 10:05:38 +0100

    used Typeof to determine the type data API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
+  const latitude = ""
+  const longitude = ""
 
   const getCoordinates = async() => {
     const response = await fetch (url)
     const data = await response.json()
-    console.log(data["iss_position"]["latitude"])
+    console.log(typeof(data["iss_position"]["latitude"]))
     console.log(data["iss_position"]["longitude"])
   }
 
